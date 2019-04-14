6da508a5d378663c22518dcd536dbf83a64b3f52
me0em <38053198+me0em@users.noreply.github.com>
2019-04-14 13:29:26 +0300

    v0.232 / database init in main file

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"memefy-server/api"
 	"net/http"
 )
-
 
 func runServer() {
 	// TODO: (GET /) func handler, HTTP/2 pushed css
 	// Configs etc
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/api/user", api.CreateUser)
-	http.HandleFunc("/api/memes", api.ThrowMemes)
-	http.HandleFunc("/api/reaction", api.HandleReaction)
+	http.HandleFunc("/api/memes", api.SendMemes)
+	http.HandleFunc("/api/reaction", api.GetReaction)
 	http.HandleFunc("/api/test", api.TestThings)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8083", nil))
 }
 
 func main() {
+	fmt.Println("Run server...")
+	api.InitDB("root@tcp(127.0.0.1:3306)/memefy?parseTime=true")
+	fmt.Println("Init the database successfully...")
 	runServer()
 }
