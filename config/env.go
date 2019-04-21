80a37e6a983655fc0eff0f8aa35bd42f75fed52d
Evgeny Kotlyarov <tyztot@gmail.com>
2019-04-21 22:45:52 +0300

    v0.267 / адрес сервера

--- config/env.go
+++ config/env.go
@@ -4,10 +4,10 @@ package config
 var ServerPort = ":8085"
 
 // DBInitReq is init database request
-var DBInitReq = "http://default::tyz1214@127.0.0.1:8123/memefy?parseTime=true"
+var DBInitReq = "http://default:@memefy.fun:8123/memefy?parseTime=true"
 
 // SecretKey is a JWT secret key for generating tokens
 var SecretKey = "wer6YTIFpojneEfe34fr4go8ukcyyjr45y8867"
 
 // MLModelHost is a host of machine learning server
-var MLModelHost = "http://127.0.0.1:5000/"
+var MLModelHost = "http://127.0.0.1:8228/"
