461d1170161fe6d903f06a64d56c03ac71bcbe1a
Evgeny Kotlyarov <tyztot@gmail.com>
2019-04-23 12:40:34 +0300

    v0.2682 / готовые файлы докера с переменными окружениями

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM frolvlad/alpine-glibc
 
+ENV SERVERPORT=":8085"
+ENV DBINITREQ="http://default@memefy.fun:8123/memefy"
+ENV SECRETKAY="wer6YTIFpojneEfe34fr4go8ukcyyjr45y8867"
+ENV MLMODELHOST="http://127.0.0.1:8228/hero"
+
 COPY ./main ./
 
 ENTRYPOINT ./main
--- config/env.go
+++ config/env.go
@@ -1,13 +1,18 @@
 package config
 
+import "os"
+
 // ServerPort is a port using for listening
-var ServerPort = ":8085"
+var ServerPort = os.Getenv("SERVERPORT")
 
 // DBInitReq is init database request
-var DBInitReq = "http://default:@memefy.fun:8123/memefy?parseTime=true"
-
+//var DBInitReq = "http://default@memefy.fun:8123/memefy"
+//var DBInitReq = "http://default@127.0.0.1:8123/memefy"
+var DBInitReq = os.Getenv("DBINITREQ")
 // SecretKey is a JWT secret key for generating tokens
-var SecretKey = "wer6YTIFpojneEfe34fr4go8ukcyyjr45y8867"
-
+//var SecretKey = "wer6YTIFpojneEfe34fr4go8ukcyyjr45y8867"
+var SecretKey = os.Getenv("SECRETKAY")
 // MLModelHost is a host of machine learning server
-var MLModelHost = "http://127.0.0.1:8228/"
+//var MLModelHost = "http://127.0.0.1:8228/"
+var MLModelHost = os.Getenv("MLMODELHOST")
+//var Port = os.Getenv("SERVERPORT")
